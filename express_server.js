53e9504775c0009f8ee09eb13024c2339644d683
paige-clark <paigeclarkillustration@gmail.com>
2022-08-08 19:08:24 +0000

    Added EJS as a template engine

--- express_server.js
+++ express_server.js
@@ -5,6 +5,7 @@
 const express = require("express");
 const app = express();
 const PORT = 3000; // default port 3000
+app.set("view engine", "ejs"); // set the view engine to EJS
 
 ////////////////////////////////////////////
 // DATABASE
