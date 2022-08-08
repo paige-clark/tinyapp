2754d9a9f9a652f1c261ba371a5d15396266540f
paige-clark <paigeclarkillustration@gmail.com>
2022-08-08 20:02:31 +0000

    Added a page for showing the long version of a shortened URL

--- express_server.js
+++ express_server.js
@@ -20,19 +20,29 @@ const urlDatabase = {
 // ROUTES
 ////////////////////////////////////////////
 
+// page says 'hello'
 app.get("/", (req, res) => {
   res.send("Hello!");
 });
 
+// shows what's in our urls object
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
 
+// EJS page that shows list of short and long URLs
 app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
 });
 
+// EJS page that displays the long url for a URL id
+app.get("/urls/:id", (req, res) => {
+  const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id] };
+  res.render("urls_show", templateVars);
+});
+
+// Says Hellow World with HTML formatting
 app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
